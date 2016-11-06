5e2e110fa6f4658a0c0eb3b78782637dbdf7281a
NguyenSyLuu <syluu.nguyen@trueplus.vn>
2016-11-06 13:16:52 +0700

    #2 Show grid r

---
+++ app/code/Magestore/Storepickup/Controller/Index/Test.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Magestore
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Magestore.com license that is
+ * available through the world-wide-web at this URL:
+ * http://www.magestore.com/license-agreement.html
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Magestore
+ * @package     Magestore_Storepickup
+ * @copyright   Copyright (c) 2012 Magestore (http://www.magestore.com/)
+ * @license     http://www.magestore.com/license-agreement.html
+ */
+
+namespace Magestore\Storepickup\Controller\Index;
+
+/**
+ * @category Magestore
+ * @package  Magestore_Storepickup
+ * @module   Storepickup
+ * @author   Magestore Developer
+ */
+class Test extends \Magestore\Storepickup\Controller\Index
+{
+    /**
+     * Execute action.
+     */
+    public function execute()
+    {
+        $model = $this->_objectManager->create('\Magestore\Storepickup\Model\ResourceModel\Store\Grid\Collection');
+
+        \Zend_Debug::dump($model->getData());
+        die();
+    }
+}
